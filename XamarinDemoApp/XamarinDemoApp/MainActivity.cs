a27d75df602f3bb118a4c3666a953ce53f25b22f
gustavomarty <gustavo.marty@hotmail.com>
2016-05-29 22:41:40 -0300

    Criação projeto demo app com xamarin

---
+++ XamarinDemoApp/XamarinDemoApp/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+
+namespace XamarinDemoApp
+{
+    [Activity(Label = "XamarinDemoApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var txtUser = FindViewById<EditText>(Resource.Id.txtUser);
+            var btnSearch = FindViewById<Button>(Resource.Id.btnSearch);
+            var lvwRepositories = FindViewById<ListView>(Resource.Id.lvwRepositories);
+            
+            btnSearch.Click += async (object sender, EventArgs e) =>
+            {
+                var github = new Shared.GitHubApi();
+                var repositories = await github.GetAsync(txtUser.Text);
+                lvwRepositories.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItemSingleChoice, repositories);
+            };
+        }
+    }
+}
+
